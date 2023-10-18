7de741668c43cfafed115bc1f7a2c5729ff2bccd
JoseMDO <joseamontesdeocamo@lewisu.edu>
2023-10-18 14:45:55 -0500

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
 app.use(express.static(__dirname + '/client'))
-
+app.use(cors());
 
 app.get('/test', function(request, response) {
 	response.type('text/plain')
