63bed323c6609d24067e72f46dbe5509d6ed3660
JoseMDO <joseamontesdeocamo@lewisu.edu>
2023-10-18 14:40:50 -0500

    updated index

--- index.js
+++ index.js
@@ -3,20 +3,23 @@ const app = express();
 
 app.use(express.static(__dirname + '/client'))
 
-const port = process.env.PORT || 3000
+
 app.get('/test', function(request, response) {
 	response.type('text/plain')
 	response.send('Node.js and Express running on port='+port)
 })
 
-app.listen(port, function() {
-	console.log("Server is running at http://localhost:3000/")
-})
-
-
+// Load your JSON data
 const favoritePlaces = require('./FavoritePlaces.json');
 
 // Create a route that serves the JSON data
 app.get('/api/favorite-places', (req, res) => {
   res.json(favoritePlaces);
 });
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
